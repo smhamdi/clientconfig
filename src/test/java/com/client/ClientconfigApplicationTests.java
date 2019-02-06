691862eaa736f54146036a06f6342bdb3343facd
safae <safae@dell-PC>
2019-02-06 23:34:12 +0100

    client config

---
+++ src/test/java/com/client/ClientconfigApplicationTests.java
@@ -0,0 +1,17 @@
+package com.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientconfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
